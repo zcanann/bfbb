01291ab9ae28eceff75e088f40bbd8990f51714d
Zachary Canann <zacharycanann@gmail.com>
2026-04-20 23:13:53 -0700

    Match MSL signal.c

--- src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/signal.c
+++ src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/signal.c
@@ -3,34 +3,34 @@
 #include "PowerPC_EABI_Support/MSL_C/MSL_Common/critical_regions.h"
 #include "PowerPC_EABI_Support/MSL_C/MSL_Common/abort_exit.h"
 
-#define SIGNAL_NUM 6
-
-__signal_func_ptr signal_funcs[SIGNAL_NUM];
+__signal_func_ptr signal_funcs[6];
 
 int raise(int sig)
 {
     __signal_func_ptr signal_func;
 
-    if (sig < 1 || sig > SIGNAL_NUM) {
+    if (sig < 1 || sig > 6)
+    {
         return -1;
     }
 
     __begin_critical_region(stderr_access);
     signal_func = signal_funcs[sig - 1];
 
-    if (signal_func != ((__std(__signal_func_ptr))1)) {
+    if (signal_func != ((__std(__signal_func_ptr))1))
+    {
         signal_funcs[sig - 1] = ((__std(__signal_func_ptr))0);
     }
 
     __end_critical_region(stderr_access);
 
-    if (signal_func == ((__std(__signal_func_ptr))1) ||
-        (signal_func == ((__std(__signal_func_ptr))0) && sig == 1))
+    if (signal_func == ((__std(__signal_func_ptr))1) || (signal_func == ((__std(__signal_func_ptr))0) && sig == 1))
     {
         return 0;
     }
 
-    if (signal_func == ((__std(__signal_func_ptr))0)) {
+    if (signal_func == ((__std(__signal_func_ptr))0))
+    {
         exit(0);
     }
 
