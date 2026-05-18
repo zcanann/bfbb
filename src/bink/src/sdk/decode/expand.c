d9f371a75b1aec2979dc42efde3184649d7814e1
Zachary Canann <zacharycanann@gmail.com>
2026-05-18 15:35:54 -0700

    Clarify expand bit count check

--- src/bink/src/sdk/decode/expand.c
+++ src/bink/src/sdk/decode/expand.c
@@ -208,7 +208,7 @@ static inline u32 exp_get_bits(EXPBITS PTR4* bits, u32 count)
 
     mask = GetBitsLen(count);
     bitcount = bits->bitlen;
-    if (bitcount > count - 1) {
+    if (bitcount >= count) {
         bitbuf = bits->bits;
         bits->bitlen = bitcount - count;
         bits->bits = bitbuf >> count;
