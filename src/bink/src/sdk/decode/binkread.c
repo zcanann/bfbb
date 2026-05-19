0df5876b648003f458b2d3b35a244eaad2b8a917
Zachary Canann <zacharycanann@gmail.com>
2026-05-18 20:09:27 -0700

    Reuse split cursor in Bink rect splitting

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -2443,10 +2443,9 @@ static s32 trysplit(BINKRECT PTR4* outa, BINKRECT PTR4* outb, const BINKRECT PTR
     BINKRECT second_half;
     BINKRECT split_rect;
     s32 best_score;
+    u32 split;
 
     if (rect->Width >= BINK_DIRTY_SPLIT_MIN_SIZE) {
-        u32 split;
-
         split_rect = *rect;
         split = (split_rect.Width / BINK_COPY_SCALE + BINK_MASK_BLOCK_ROUND_MASK) &
                 ~BINK_MASK_BLOCK_ROUND_MASK;
@@ -2465,7 +2464,6 @@ static s32 trysplit(BINKRECT PTR4* outa, BINKRECT PTR4* outb, const BINKRECT PTR
 
     if (rect->Height >= BINK_DIRTY_SPLIT_MIN_SIZE) {
         s32 split_score;
-        u32 split;
 
         split_rect = *rect;
         split = (split_rect.Height / BINK_COPY_SCALE + BINK_MASK_BLOCK_ROUND_MASK) &
