dada23e2927049f529624d857df2b3e5441621f3
Zachary Canann <zacharycanann@gmail.com>
2026-05-20 11:09:59 -0700

    Limit ngc sound constants to file scope

--- src/bink/src/sdk/decode/ngc/ngcsnd.c
+++ src/bink/src/sdk/decode/ngc/ngcsnd.c
@@ -154,17 +154,14 @@ typedef char NGCSoundStateFitsInBinkSndData
 #define AX_VOICE_MIX_MODE(voice) ((voice)->pb.mixerCtrl)
 #define AX_VOICE_SYNC_FLAGS(voice) ((voice)->sync)
 
-extern const f64 BINK_NGC_SOUND_U32_TO_F64_BIAS;
-extern const f64 BINK_NGC_SOUND_SIGN_BIAS;
-
-const f32 BINK_NGC_PAN_ONE = 1.0f;
-const f32 BINK_NGC_PAN_EXPONENT = 0.3f;
-const f32 BINK_NGC_MIX_SCALE = 65535.0f;
-const f64 BINK_NGC_U32_TO_F64_BIAS = 4503599627370496.0;
-const f32 BINK_NGC_AX_SAMPLE_RATE = 32000.0f;
-const f64 BINK_NGC_S32_TO_F64_BIAS = 4503601774854144.0;
-const f32 BINK_NGC_PAN_TO_FLOAT = 0.0000152587890625f;
-const f32 BINK_NGC_PAN_CENTER = 0.5f;
+static const f32 BINK_NGC_PAN_ONE = 1.0f;
+static const f32 BINK_NGC_PAN_EXPONENT = 0.3f;
+static const f32 BINK_NGC_MIX_SCALE = 65535.0f;
+static const f64 BINK_NGC_U32_TO_F64_BIAS = 4503599627370496.0;
+static const f32 BINK_NGC_AX_SAMPLE_RATE = 32000.0f;
+static const f64 BINK_NGC_S32_TO_F64_BIAS = 4503601774854144.0;
+static const f32 BINK_NGC_PAN_TO_FLOAT = 0.0000152587890625f;
+static const f32 BINK_NGC_PAN_CENTER = 0.5f;
 
 #define NGC_SOUND_PAN_ONE BINK_NGC_PAN_ONE
 #define NGC_SOUND_PAN_EXPONENT BINK_NGC_PAN_EXPONENT
@@ -182,8 +179,8 @@ const char BINK_ERROR_NOT_BINK[20] = "Not a Bink file.";
 const char BINK_ERROR_NO_COMPRESSED_FRAMES[] = "The file doesn't contain any compressed frames yet.";
 const char BINK_ERROR_OUT_OF_MEMORY[20] = "Out of memory.";
 
-const f64 BINK_NGC_SOUND_U32_TO_F64_BIAS = 4503599627370496.0;
-const f64 BINK_NGC_SOUND_SIGN_BIAS = 2147483648.0;
+static const f64 BINK_NGC_SOUND_U32_TO_F64_BIAS = 4503599627370496.0;
+static const f64 BINK_NGC_SOUND_SIGN_BIAS = 2147483648.0;
 
 static void startVoices(u32 task)
 {
