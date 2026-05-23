49efc4d7b27e1c82027e13f09fd608c2ae6eeb53
Zachary Canann <zacharycanann@gmail.com>
2026-05-23 11:20:13 -0700

    Simplify NGC simulated read delay

--- src/bink/src/sdk/decode/ngc/ngcfile.c
+++ src/bink/src/sdk/decode/ngc/ngcfile.c
@@ -226,13 +226,11 @@ static u32 BinkFileReadHeader(BINKIO PTR4* io, s32 offset, void PTR4* dest, u32 
 static void dosimulate(BINKIO PTR4* io, u32 read_size, u32 start)
 {
     u32 last;
-    u32 elapsed;
     s32 delay;
 
     delay = mult64anddiv(read_size, NGC_MILLISECONDS_PER_SECOND, NGC_SIMULATE_RATE(io));
     last = RADTimerRead();
-    elapsed = last - start;
-    delay -= elapsed;
+    delay -= last - start;
     NGC_SIMULATE_DELAY(io) += delay;
 
     while (NGC_SIMULATE_DELAY(io) > 0) {
