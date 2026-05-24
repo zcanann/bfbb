7715fb7264497157c114861af49eb96e5ce1bc76
Zachary Canann <zacharycanann@gmail.com>
2026-05-23 18:55:17 -0700

    Simplify NGC DVD idle status handling

--- src/bink/src/sdk/decode/ngc/ngcfile.c
+++ src/bink/src/sdk/decode/ngc/ngcfile.c
@@ -115,7 +115,7 @@ u32 RGBshift[RGB_SHIFT_TABLE_SIZE] RAD_ATTRIBUTE_ALIGN(NGC_TABLE_ALIGNMENT) = { 
     ((u32)((status) - DVD_STATE_BUSY) <= (DVD_STATE_WAITING - DVD_STATE_BUSY))
 #define NGC_DVD_STATUS_FAILED(status)                                                              \
     ((status) <= DVD_STATE_IGNORED ?                                                               \
-         (DVD_STATE_WAITING < (status) || (status) == DVD_STATE_FATAL_ERROR) :                     \
+         ((status) > DVD_STATE_WAITING || (status) == DVD_STATE_FATAL_ERROR) :                     \
          (status) == DVD_STATE_RETRY)
 
 static void ReadKickoff(BINKIO PTR4* io);
@@ -225,14 +225,12 @@ static u32 BinkFileReadHeader(BINKIO PTR4* io, s32 offset, void PTR4* dest, u32 
 
 static void dosimulate(BINKIO PTR4* io, u32 read_size, u32 start)
 {
-    u32 last;
-    u32 elapsed;
     s32 delay;
+    u32 last;
 
     delay = mult64anddiv(read_size, NGC_MILLISECONDS_PER_SECOND, NGC_SIMULATE_RATE(io));
     last = RADTimerRead();
-    elapsed = last - start;
-    delay -= elapsed;
+    delay -= last - start;
     NGC_SIMULATE_DELAY(io) += delay;
 
     while (NGC_SIMULATE_DELAY(io) > 0) {
@@ -369,14 +367,18 @@ static u32 BinkFileIdle(BINKIO PTR4* io)
         return io->DoingARead;
     }
 
-    if (status <= DVD_STATE_END) {
-        if (status == DVD_STATE_FATAL_ERROR) {
-            goto read_error;
-        }
-        if (status == DVD_STATE_END) {
-            ReadKickoff(io);
-        }
+    if (status > DVD_STATE_END) {
+        return io->DoingARead;
     }
+
+    if (status == DVD_STATE_FATAL_ERROR) {
+        goto read_error;
+    }
+
+    if (status == DVD_STATE_END) {
+        ReadKickoff(io);
+    }
+
     return io->DoingARead;
 
 read_error:
