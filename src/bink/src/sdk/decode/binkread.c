222139bded5278287cbc6caede0e43392e1f07a4
Zachary Canann <zacharycanann@gmail.com>
2026-05-23 18:46:01 -0700

    Clarify Bink file and bundle conditions

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -2361,10 +2361,12 @@ static s32 smallestrect(BINKRECT PTR4* out, const u8 PTR4* mask, s32 pitch, cons
     s32 row;
     s32 col;
     s32 value;
+    const BINKRECT PTR4* src_rect;
 
-    rect_mask = mask + BINK_MASK_BLOCKS(rect->Left) + BINK_MASK_BLOCKS(rect->Top) * pitch;
-    height_blocks = BINK_MASK_BLOCKS(rect->Height);
-    width_blocks = BINK_MASK_BLOCKS(rect->Width);
+    src_rect = rect;
+    rect_mask = mask + BINK_MASK_BLOCKS(src_rect->Left) + BINK_MASK_BLOCKS(src_rect->Top) * pitch;
+    height_blocks = BINK_MASK_BLOCKS(src_rect->Height);
+    width_blocks = BINK_MASK_BLOCKS(src_rect->Width);
 
     top_scan = rect_mask;
 
@@ -2383,8 +2385,8 @@ static s32 smallestrect(BINKRECT PTR4* out, const u8 PTR4* mask, s32 pitch, cons
 found_top:
     rect_mask += top_blocks * pitch;
     height_blocks -= top_blocks;
-    out->Top = rect->Top + top_blocks * BINK_MASK_BLOCK_SIZE;
-    value = rect->Height - top_blocks * BINK_MASK_BLOCK_SIZE;
+    out->Top = src_rect->Top + top_blocks * BINK_MASK_BLOCK_SIZE;
+    value = src_rect->Height - top_blocks * BINK_MASK_BLOCK_SIZE;
     out->Height = value;
 
     if (value > 1) {
@@ -2422,8 +2424,8 @@ found_bottom:
 found_left:
     width_blocks -= left_blocks;
     rect_mask += left_blocks;
-    out->Left = rect->Left + left_blocks * BINK_MASK_BLOCK_SIZE;
-    value = rect->Width - left_blocks * BINK_MASK_BLOCK_SIZE;
+    out->Left = src_rect->Left + left_blocks * BINK_MASK_BLOCK_SIZE;
+    value = src_rect->Width - left_blocks * BINK_MASK_BLOCK_SIZE;
     out->Width = value;
 
     if (value > 1) {
