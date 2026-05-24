51418c646061a5cd0c7979f2b7b201a5798f56b8
Zachary Canann <zacharycanann@gmail.com>
2026-05-24 11:11:45 -0700

    Use counted loop for Bink Huff4 bundle

--- src/bink/src/sdk/decode/expand.c
+++ src/bink/src/sdk/decode/expand.c
@@ -680,10 +680,10 @@ static void CheckReadHuff4Bundle(READBUNDLE PTR4* bundle, EXPBITS PTR4* bits)
             values = bundle->values;
             decode = bundle->decode;
             peek = bundle->bits_to_peek;
-            while (count != 0) {
+            count--;
+            do {
                 *dest++ = (u8)exp_read_huff4(bits, peek, decode, values);
-                count--;
-            }
+            } while (count-- != 0);
         } else {
             value = exp_get_bits(bits, HUFF4_USED_SHIFT);
             memset(bundle->data, value, count);
