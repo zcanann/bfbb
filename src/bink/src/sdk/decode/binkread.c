90c7bb8d260c793aca8d8623cb61a2c26d5a9d40
Zachary Canann <zacharycanann@gmail.com>
2026-05-27 19:51:39 -0700

    Clarify Bink callback and bundle flags

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -712,9 +712,8 @@ static u32 dosilence(BINKSND PTR4* snd)
 static void checksound(HBINK bnk)
 {
     if (bnk->bsnd[bnk->playingtracks - 1].Ready != 0) {
-        RADCB_CALLBACK PTR4* callback = BINK_SOUND_CALLBACK(bnk);
-
-        if (RADCB_try_to_suspend_callback(cb_bink_sound, callback) != 0) {
+        if (RADCB_try_to_suspend_callback(cb_bink_sound, BINK_SOUND_CALLBACK(bnk)) != 0) {
+            RADCB_CALLBACK PTR4* callback = BINK_SOUND_CALLBACK(bnk);
             u32 i;
 
             i = 0;
