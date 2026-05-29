db90df8b37507c31e8904f603b0caa4f96008afd
Zachary Canann <zacharycanann@gmail.com>
2026-05-28 20:18:19 -0700

    Simplify NGC sync read status loop

--- src/bink/src/sdk/decode/ngc/ngcfile.c
+++ src/bink/src/sdk/decode/ngc/ngcfile.c
@@ -173,15 +173,13 @@ static u32 radreadngc(DVDFileInfo PTR4* file, u32 offset, void PTR4* dest, u32 s
                 break;
             }
             if (status <= DVD_STATE_END) {
-                if (status != DVD_STATE_FATAL_ERROR) {
-                    continue;
+                if (status == DVD_STATE_FATAL_ERROR) {
+                    return 0;
                 }
-                return 0;
             } else if (status <= DVD_STATE_RETRY) {
-                if (status <= DVD_STATE_WAITING) {
-                    continue;
+                if (status > DVD_STATE_WAITING) {
+                    return 0;
                 }
-                return 0;
             }
 
         } while (1);
