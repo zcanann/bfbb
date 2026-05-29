dacc7c4ed75adc5ae25ea0ad313a8aee576c68e9
Zachary Canann <zacharycanann@gmail.com>
2026-05-28 19:56:37 -0700

    Simplify Bink audio bit tests

--- src/bink/src/sdk/decode/binkacd.c
+++ src/bink/src/sdk/decode/binkacd.c
@@ -239,7 +239,7 @@ static void read_rle_samples(f32 PTR4* samples, u32 transform_size, VARBITS PTR4
 
         /* Each sparse coefficient packet is either 5 bits (literal VQ run) or
            9 bits (RLE flag, 4-bit run index, 4-bit coefficient bit length). */
-        if (read_bit(vb) != 0) {
+        if (read_bit(vb)) {
             u32 run = read_rle_bits(vb);
 
             end = i + BINKAC_RLE_SAMPLE_RUN(run);
@@ -271,7 +271,7 @@ static void read_rle_samples(f32 PTR4* samples, u32 transform_size, VARBITS PTR4
                 {
                     s32 value = read_bits(vb, bitlen);
 
-                    if (value != 0) {
+                    if (value) {
                         /* Bink audio 1 stores the sign bit after each nonzero coefficient. */
                         s32 sign = -(s32)read_bit(vb);
                         value = (value ^ sign) - sign;
