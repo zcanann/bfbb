9567bfe5fb37701110b75c12d2308e4bbd89cd97
Zachary Canann <zacharycanann@gmail.com>
2026-05-28 19:08:33 -0700

    Use continue for radreadngc polling states

--- src/bink/src/sdk/decode/ngc/ngcfile.c
+++ src/bink/src/sdk/decode/ngc/ngcfile.c
@@ -173,13 +173,15 @@ static u32 radreadngc(DVDFileInfo PTR4* file, u32 offset, void PTR4* dest, u32 s
                 break;
             }
             if (status <= DVD_STATE_END) {
-                if (status == DVD_STATE_FATAL_ERROR) {
-                    return 0;
+                if (status != DVD_STATE_FATAL_ERROR) {
+                    continue;
                 }
+                return 0;
             } else if (status <= DVD_STATE_RETRY) {
-                if (status > DVD_STATE_WAITING) {
-                    return 0;
+                if (status <= DVD_STATE_WAITING) {
+                    continue;
                 }
+                return 0;
             }
 
         } while (1);
