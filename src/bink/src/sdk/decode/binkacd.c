e935e24a29c723b494bdf72e67f65383f796e009
Zachary Canann <zacharycanann@gmail.com>
2026-05-29 09:40:13 -0700

    Name Bink audio RLE packet local

--- src/bink/src/sdk/decode/binkacd.c
+++ src/bink/src/sdk/decode/binkacd.c
@@ -253,12 +253,16 @@ static void read_rle_samples(f32 PTR4* samples, u32 transform_size, VARBITS PTR4
 
         /* Each sparse coefficient packet is either 5 bits (literal VQ run) or
            9 bits (RLE flag, 4-bit run index, 4-bit coefficient bit length). */
-        if (read_bit(vb)) {
-            u32 run = read_rle_bits(vb);
+        {
+            u32 rle_packet = read_bit(vb);
 
-            end = i + BINKAC_RLE_SAMPLE_RUN(run);
-        } else {
-            end = i + VQLENGTH;
+            if (rle_packet) {
+                u32 run = read_rle_bits(vb);
+
+                end = i + BINKAC_RLE_SAMPLE_RUN(run);
+            } else {
+                end = i + VQLENGTH;
+            }
         }
 
         if (end > transform_size) {
