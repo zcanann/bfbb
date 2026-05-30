927796852cf0863b9625925e6bceb12e97eba1cc
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 15:11:07 -0700

    Use pointer add in high rate scan

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -948,7 +948,7 @@ static u32 high1secrate(s32 frames, const u32 PTR4* frameoffsets, s32 span,
     i = 0;
     if (i < frames - span) {
         const u32 PTR4* start = frameoffsets;
-        const u32 PTR4* end = &frameoffsets[span];
+        const u32 PTR4* end = frameoffsets + span;
 
         do {
             u32 diff = *end++ - *start++;
