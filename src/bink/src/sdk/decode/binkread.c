3c937f3ec5f3440d7d694da3d2b8e07e7fc9c698
Zachary Canann <zacharycanann@gmail.com>
2026-05-30 19:36:28 -0700

    Return Bink split score directly

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -2554,10 +2554,9 @@ static s32 trysplit(BINKRECT PTR4* outa, BINKRECT PTR4* outb, const BINKRECT PTR
         split_score = (BINK_RECT_AREA(rect) - BINK_RECT_AREA(&first_half)) -
                       BINK_RECT_AREA(&second_half);
         if (split_score > best_score) {
-            best_score = split_score;
             *outa = first_half;
             *outb = second_half;
-            return best_score;
+            return split_score;
         }
     }
 
