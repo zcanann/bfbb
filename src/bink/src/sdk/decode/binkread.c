d338af87089f4d11a74fa2b9c70fec357ba2918c
Zachary Canann <zacharycanann@gmail.com>
2026-06-01 13:49:27 -0700

    Inline Bink sound table allocation size

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -1026,7 +1026,6 @@ HBINK BinkOpen(const char PTR4* name, u32 flags)
     BINKIOOPEN open;
     u32 scale_flags;
     u32 all_key;
-    u32 sound_bytes;
     u32 simulate;
     HBINK out;
 
@@ -1156,11 +1155,8 @@ HBINK BinkOpen(const char PTR4* name, u32 flags)
                BINK_ARRAY_BYTES(bnk.runtimeframes, bnk.rtidlereadtimes));
     pushmalloc((void PTR4* PTR4*)&bnk.rtthreadreadtimes,
                BINK_ARRAY_BYTES(bnk.runtimeframes, bnk.rtthreadreadtimes));
-    sound_bytes = sizeof(*bnk.bsnd);
-    if (TotTracks != 0) {
-        sound_bytes = BINK_ARRAY_BYTES(TotTracks, bnk.bsnd);
-    }
-    pushmalloc((void PTR4* PTR4*)&bnk.bsnd, sound_bytes);
+    pushmalloc((void PTR4* PTR4*)&bnk.bsnd,
+               TotTracks == 0 ? sizeof(*bnk.bsnd) : BINK_ARRAY_BYTES(TotTracks, bnk.bsnd));
     pushmalloc((void PTR4* PTR4*)&bnk.trackindexes,
                BINK_ARRAY_BYTES(TotTracks, bnk.trackindexes));
 
