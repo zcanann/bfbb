32c62849cd64d8fcd6e76ced713fa5bbfa2a8294
Zachary Canann <zacharycanann@gmail.com>
2026-06-01 15:21:58 -0700

    Drop redundant Bink rect alias

--- src/bink/src/sdk/decode/binkread.c
+++ src/bink/src/sdk/decode/binkread.c
@@ -2415,7 +2415,6 @@ void BinkGetRealtime(HBINK bink, BINKREALTIME PTR4* run, u32 frames)
 
 static s32 smallestrect(BINKRECT PTR4* out, const u8 PTR4* mask, s32 pitch, const BINKRECT PTR4* rect)
 {
-    const BINKRECT PTR4* input;
     s32 width_blocks;
     s32 height_blocks;
     const u8 PTR4* rect_mask;
@@ -2426,10 +2425,9 @@ static s32 smallestrect(BINKRECT PTR4* out, const u8 PTR4* mask, s32 pitch, cons
     s32 col;
     s32 value;
 
-    input = rect;
-    rect_mask = mask + BINK_MASK_BLOCKS(input->Left) + BINK_MASK_BLOCKS(input->Top) * pitch;
-    height_blocks = BINK_MASK_BLOCKS(input->Height);
-    width_blocks = BINK_MASK_BLOCKS(input->Width);
+    rect_mask = mask + BINK_MASK_BLOCKS(rect->Left) + BINK_MASK_BLOCKS(rect->Top) * pitch;
+    height_blocks = BINK_MASK_BLOCKS(rect->Height);
+    width_blocks = BINK_MASK_BLOCKS(rect->Width);
 
     top_scan = rect_mask;
 
@@ -2448,8 +2446,8 @@ static s32 smallestrect(BINKRECT PTR4* out, const u8 PTR4* mask, s32 pitch, cons
 found_top:
     rect_mask += top_blocks * pitch;
     height_blocks -= top_blocks;
-    out->Top = input->Top + top_blocks * BINK_MASK_BLOCK_SIZE;
-    value = input->Height - top_blocks * BINK_MASK_BLOCK_SIZE;
+    out->Top = rect->Top + top_blocks * BINK_MASK_BLOCK_SIZE;
+    value = rect->Height - top_blocks * BINK_MASK_BLOCK_SIZE;
     out->Height = value;
 
     if (value > 1) {
@@ -2487,8 +2485,8 @@ found_bottom:
 found_left:
     width_blocks -= left_blocks;
     rect_mask += left_blocks;
-    out->Left = input->Left + left_blocks * BINK_MASK_BLOCK_SIZE;
-    value = input->Width - left_blocks * BINK_MASK_BLOCK_SIZE;
+    out->Left = rect->Left + left_blocks * BINK_MASK_BLOCK_SIZE;
+    value = rect->Width - left_blocks * BINK_MASK_BLOCK_SIZE;
     out->Width = value;
 
     if (value > 1) {
