e6e26af6d7b8baa83d2e2c8b6b5bd68f90dbd2b5
Zachary Canann <zacharycanann@gmail.com>
2026-06-03 23:09:05 -0700

    Derive cftf161 twiddle pair

--- src/bink/src/sdk/fft.c
+++ src/bink/src/sdk/fft.c
@@ -1766,7 +1766,8 @@ static void cftf161(f32 PTR4* a, f32 PTR4* w)
 
     wn4r = w[1];
     wk1r = w[2];
-    wk1i = w[3];
+    wk1i = wn4r * wk1r;
+    wk1r = wk1i + wk1r;
     x0r = a[0] + a[16];
     x0i = a[1] + a[17];
     x1r = a[0] - a[16];
