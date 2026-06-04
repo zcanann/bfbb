eb372847964bc496a1906332358ae765517dcb0a
Zachary Canann <zacharycanann@gmail.com>
2026-06-03 21:55:03 -0700

    Use u64 cycle timer delta local

--- src/bink/src/sdk/decode/ngc/binkngc.c
+++ src/bink/src/sdk/decode/ngc/binkngc.c
@@ -224,9 +224,9 @@ void RADCycleTimerStartAddr64(u64 PTR4* dest)
 
 void RADCycleTimerDeltaAddr64(u64 PTR4* dest)
 {
-    RADTimebase now;
-    radtimebase(&now);
-    *dest = *(u64 PTR4*)&now - *dest;
+    u64 now;
+    radtimebase((RADTimebase PTR4*)&now);
+    *dest = now - *dest;
 }
 
 void ReadTimeBase(u32 PTR4* dest)
