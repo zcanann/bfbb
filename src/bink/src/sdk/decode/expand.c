dacdb571ab51fba86e436aaec3b6800afb513956
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 18:16:13 -0700

    Name Bink pattern color bit

--- src/bink/src/sdk/decode/expand.c
+++ src/bink/src/sdk/decode/expand.c
@@ -30,6 +30,7 @@ typedef enum BINKPlaneLayout
     BINK_PATTERN_BLOCK_BYTES = BINK_BLOCK_SIDE,
     BINK_PATTERN_COLOR_0 = 0,
     BINK_PATTERN_COLOR_1 = 1,
+    BINK_PATTERN_COLOR_BIT = 1,
     BINK_PATTERN_COLOR_COUNT = 2,
     BINK_RUN_BLOCK_BYTES = 0x30
 } BINKPlaneLayout;
@@ -1046,7 +1047,7 @@ static inline void expand_pattern_block(u8 PTR4* dest,
 
         row_bits = *patterns->cur_ptr++;
         for (col = 0; col < BINK_BLOCK_SIDE; ++col) {
-            dest[i * pitch + col] = (row_bits & 1) != 0 ? color1 : color0;
+            dest[i * pitch + col] = (row_bits & BINK_PATTERN_COLOR_BIT) != 0 ? color1 : color0;
             row_bits >>= 1;
         }
     }
