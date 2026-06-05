5eabd0253a23d8a1d717712522c8593dfa02714e
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 21:39:49 -0700

    Unify plain IDCT column flow

--- src/bink/src/sdk/dct.c
+++ src/bink/src/sdk/dct.c
@@ -872,19 +872,20 @@ static void fastidct8x8(u8 PTR4* dest, s32 pitch, s16 PTR4* in, const s32 PTR4* 
             out[DCT_ROW2] = dc;
             out[DCT_ROW1] = dc;
         } else {
-            s32 row2 = DCT_DEQUANT(in, q, DCT_ROW2);
-            s32 row6 = DCT_DEQUANT(in, q, DCT_ROW6);
             s32 row1 = DCT_DEQUANT(in, q, DCT_ROW1);
             s32 row3 = DCT_DEQUANT(in, q, DCT_ROW3);
             s32 row5 = DCT_DEQUANT(in, q, DCT_ROW5);
-            s32 even_sum = row2 + row6;
             s32 row7 = DCT_DEQUANT(in, q, DCT_ROW7);
             s32 odd0 = row5 - row3;
             s32 odd1 = row1 - row7;
+            s32 odd_sum = odd0 + odd1;
             s32 odd_pair0 = row1 + row7;
             s32 odd_pair1 = row5 + row3;
+            s32 row2 = DCT_DEQUANT(in, q, DCT_ROW2);
+            s32 row6 = DCT_DEQUANT(in, q, DCT_ROW6);
             s32 even_diff = DCT_FIXED_MUL(row2 - row6, DCT_IDCT_A1) - (row2 + row6);
-            s32 odd_rot = DCT_FIXED_MUL(odd0 + odd1, DCT_IDCT_A3);
+            s32 even_sum = row2 + row6;
+            s32 odd_rot = DCT_FIXED_MUL(odd_sum, DCT_IDCT_A3);
             s32 odd_mid = (DCT_FIXED_MUL(odd0, DCT_IDCT_A4) + odd_rot) - (odd_pair0 + odd_pair1);
             s32 odd_cross = DCT_FIXED_MUL(odd_pair0 - odd_pair1, DCT_IDCT_A1) - odd_mid;
             s32 odd_tail = (DCT_FIXED_MUL(odd1, DCT_IDCT_A2) - odd_rot) + odd_cross;
