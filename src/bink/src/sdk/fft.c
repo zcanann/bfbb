56dcf3e7f735fc3b59d2777ba5d8f22a18883ec7
Zachary Canann <zacharycanann@gmail.com>
2026-06-04 23:04:49 -0700

    Clarify inverse CFT twiddle initialization

--- src/bink/src/sdk/fft.c
+++ src/bink/src/sdk/fft.c
@@ -1145,8 +1145,8 @@ static void cftb1st(s32 n, f32 PTR4* a, f32 PTR4* w)
     csc3 = w[3];
     wd1r = CFT_INV_ROT_ONE;
     wd1i = CFT_INV_ROT_ZERO;
-    wd3r = wd1r;
-    wd3i = wd1i;
+    wd3r = CFT_INV_ROT_ONE;
+    wd3i = CFT_INV_ROT_ZERO;
     k = 0;
     for (j = 2; j < mh - 2; j += 4)
     {
