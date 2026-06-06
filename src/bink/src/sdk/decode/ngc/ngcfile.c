a7d7265d3bbd2f365364fbb0f0e84d5e156ff162
Zachary Canann <zacharycanann@gmail.com>
2026-06-05 18:55:52 -0700

    Use DVD cover lower bound in radreadngc

--- src/bink/src/sdk/decode/ngc/ngcfile.c
+++ src/bink/src/sdk/decode/ngc/ngcfile.c
@@ -203,9 +203,10 @@ static u32 radreadngc(DVDFileInfo PTR4* file, u32 offset, void PTR4* dest, u32 s
                 continue;
             }
             if (status <= DVD_STATE_RETRY) {
-                if (status >= DVD_STATE_COVER_CLOSED) {
-                    return 0;
+                if (status < DVD_STATE_COVER_CLOSED) {
+                    continue;
                 }
+                return 0;
             }
             continue;
 
